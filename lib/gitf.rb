432bfd760a78ab5c24041725529dd369571a1469
thomasfrl <feral.thomas@gmail.com>
2019-01-18 18:44:36 +0100

    premier commit

--- lib/gitf.rb
+++ lib/gitf.rb
@@ -6,8 +6,11 @@ end
 def perform
 	check_if_user_gave_input
 	ssh = ARGV.join
-	binding.pry 
-	system("git add . && git commit -am \"premier commit\" && git remote add origin\"#{ssh}\" && git push orign master")
+	#binding.pry 
+	system("git add . ")
+	system ("git commit -am \"premier commit\" ")
+	system("git remote add origin",ssh)
+	system("git push orign master")
 end
 
 perform
