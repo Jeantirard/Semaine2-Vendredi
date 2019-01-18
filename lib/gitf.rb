0a6bab18b321ba3186398a604e2da58324b71d6c
thomasfrl <feral.thomas@gmail.com>
2019-01-18 18:36:32 +0100

    premier commit

--- lib/gitf.rb
+++ lib/gitf.rb
@@ -1,10 +1,12 @@
+require "pry"
 def check_if_user_gave_input
   abort("Vous devez donner le lien SSH") if (ARGV.empty? || ARGV.size >1)
 end
 
 def perform
 	check_if_user_gave_input
-	ssh = ARGV
+	ssh = ARGV.join
+	binding.pry 
 	system("git add . && git commit -am \"premier commit\" && git remote add origin\"#{ssh}\" && git push orign master")
 end
 
